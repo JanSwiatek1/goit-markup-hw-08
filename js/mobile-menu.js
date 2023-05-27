46fd77dba26c69ea29d54969d77647c210b97991
Jan Świątek <swiatek-jan@wp.pl>
2023-05-27 21:20:03 +0200

    v1

---
+++ js/mobile-menu.js
@@ -0,0 +1,9 @@
+  
+// const openMenuBtn = document.querySelector(selectors: )
+// const closeMenuBtn= document.querySelector(selectors:)
+
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+// window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+// if (!e.matches) return;
+// mobileMenu.classList.remove('is-open');
